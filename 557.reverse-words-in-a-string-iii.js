9f80fdc933dfc29123f05187503840e4664fab88
Irving Arreola Palacios <irving.a789@gmail.com>
2023-03-25 14:58:57 -0500

    557 reverse words in a string

---
+++ 557.reverse-words-in-a-string-iii.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=557 lang=javascript
+ *
+ * [557] Reverse Words in a String III
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s.split(" ").map(word => {
+        return word.split("").reverse().join("")
+    }).join(" ")
+
+};
+// @lc code=end
+
